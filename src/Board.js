409dd76558e4280469d21794465849eaa7ba6b5b
Yonghao Shu <123shuyonghao@gmail.com>
2022-05-04 23:23:37 -0700

    Basic functionality implementation

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,64 @@ import Square from "./Square";
 import React from "react";
 
 export default class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            isXNext: true
+        }
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (this.calculateWinner() !== null || squares[i] != null) {
+            return;
+        }
+        squares[i] = this.state.isXNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            isXNext: !this.state.isXNext
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square 
+                value = {this.state.squares[i]}
+                onClick = {() => this.handleClick(i)}
+            />
+        );
+    }
+
+    calculateWinner() {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        const curBoard = this.state.squares;
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (curBoard[a] !== null && curBoard[a] === curBoard[b] && curBoard[a] === curBoard[c]) {
+                return curBoard[a];
+            }
+        }
+        return null;
+      }
+
     render() {
-        const status = 'Next player: X';
+        const winner = this.calculateWinner()
+        let status;
+        if (winner !== null) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.isXNext ? 'X' : 'O');
+        }
     
         return (
             <div>
