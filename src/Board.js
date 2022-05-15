c1c2f5e03a557d4fca5b481c812101751d4b2fe0
Yonghao Shu <123shuyonghao@gmail.com>
2022-05-14 23:51:05 -0700

    Add gaming history

--- src/Board.js
+++ src/Board.js
@@ -2,68 +2,18 @@ import Square from "./Square";
 import React from "react";
 
 export default class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            isXNext: true
-        }
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.calculateWinner() !== null || squares[i] != null) {
-            return;
-        }
-        squares[i] = this.state.isXNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            isXNext: !this.state.isXNext
-        });
-    }
-
     renderSquare(i) {
         return (
             <Square 
-                value = {this.state.squares[i]}
-                onClick = {() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
-    calculateWinner() {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        const curBoard = this.state.squares;
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (curBoard[a] !== null && curBoard[a] === curBoard[b] && curBoard[a] === curBoard[c]) {
-                return curBoard[a];
-            }
-        }
-        return null;
-      }
-
     render() {
-        const winner = this.calculateWinner()
-        let status;
-        if (winner !== null) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.isXNext ? 'X' : 'O');
-        }
-    
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
